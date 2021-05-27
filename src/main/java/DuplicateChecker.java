a191eced467bcf73a5f8e6ce687444fa5b45cc8e
19Pawel97 <19pawel9772@gmail.com>
2021-05-27 21:51:43 +0200

    DuplicateChecker implementation

---
+++ src/main/java/DuplicateChecker.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class DuplicateChecker {
+
+    public static void main(String[] args) {
+
+        if (checkDuplicates()) {
+            System.out.println("There are no duplicates.");
+        } else {
+            System.out.println("There are duplicates.");
+        }
+    }
+
+    private static boolean checkDuplicates() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter letters: ");
+        String line = scanner.nextLine();
+
+        String[] letters = line.split("");
+        List<String> lettersList = new ArrayList<>(Arrays.asList(letters));
+        Set<String> lettersSet = new HashSet<>(lettersList);
+
+        if (lettersList.size() == lettersSet.size()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
+
