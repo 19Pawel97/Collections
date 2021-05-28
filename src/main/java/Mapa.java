e3884c1548834799bf0ac113b8371122f8b4ecf1
19Pawel97 <19pawel9772@gmail.com>
2021-05-28 14:39:25 +0200

    Map

---
+++ src/main/java/Mapa.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map {
+
+    public static void main(String[] args) {
+        java.util.Map<String, Citizen> map = new HashMap<>();
+    }
+
+}
